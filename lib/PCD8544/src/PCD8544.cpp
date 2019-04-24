fc54cf714986e7de6e0feec213c4bef5a50847b7
Pavlenko Eugeniy <pavlenko.obs@gmail.com>
2019-04-24 16:54:47 +0300

    update set display mode methods

--- lib/PCD8544/src/PCD8544.cpp
+++ lib/PCD8544/src/PCD8544.cpp
@@ -1,29 +1,41 @@
 #include "PCD8544.h"
 
-PCD8544::PCD8544(void (* setMode) (uint8_t), void (* setData) (uint8_t)): setMode(setMode), setData(setData)
+PCD8544::PCD8544(void (* setMode) (uint8_t), void (* setData) (uint8_t)): _setMode(setMode), _setData(setData)
 {}
+
+void PCD8544::_setDisplayMode(PCD8544_DisplayMode_t mode)
+{
+    this->_setData(0x08 | mode);
+}
 
 void PCD8544::initialize()
 {
-    this->setMode(PCD8544_DC_COMMAND);  // <-- Set command transfer mode
+    this->_setMode(PCD8544_DC_COMMAND);  // <-- Set command transfer mode
 
-    this->setData(PCD8544_FUNCTION_SET(0, 0, 1));  // <-- Tell LCD extended commands follow
-    this->setData(PCD8544_VOP(0x30));              // <-- Set LCD Vop (Contrast)
-    this->setData(PCD8544_TEMP_COEFFICIENT(0x03)); // <-- Set Temp coefficient
-    this->setData(PCD8544_BIAS(0x04));             // <-- Set LCD bias mode 1:48 (can try 0x03)
+    this->_setData(PCD8544_FUNCTION_SET(0, 0, 1));  // <-- Tell LCD extended commands follow
+    this->_setData(PCD8544_VOP(0x30));              // <-- Set LCD Vop (Contrast)
+    this->_setData(PCD8544_TEMP_COEFFICIENT(0x03)); // <-- Set Temp coefficient
+    this->_setData(PCD8544_BIAS(0x04));             // <-- Set LCD bias mode 1:48 (can try 0x03)
 
-    this->setData(PCD8544_FUNCTION_SET(0, 0, 0));  // <-- Tell LCD normal commands follow
-    this->setData(PCD8544_DISPLAY_CONTROL(1, 0));  // <-- Set display normal mode, not inverse
+    this->_setData(PCD8544_FUNCTION_SET(0, 0, 0));  // <-- Tell LCD normal commands follow
+    this->_setDisplayMode(PCD8544_DISPLAY_MODE_NORMAL);
+}
+
+void PCD8544::setDisplayMode(PCD8544_DisplayMode_t mode)
+{
+    this->_setMode(PCD8544_DC_COMMAND);
+    this->_setDisplayMode(mode);
 }
 
 void PCD8544::setCol(uint8_t col)
 {
-    this->setMode(PCD8544_DC_COMMAND);
-    this->setData(0x80 | (0x07 & col));
+    this->_setMode(PCD8544_DC_COMMAND);
+    this->_setData(0x80 | (0x07 & col));
 }
 
 void PCD8544::setRow(uint8_t row)
 {
-    this->setMode(PCD8544_DC_COMMAND);
-    this->setData(0x40 | (0x3F & row));
+    this->_setMode(PCD8544_DC_COMMAND);
+    this->_setData(0x40 | (0x3F & row));
 }
+
--- lib/PCD8544/src/PCD8544.h
+++ lib/PCD8544/src/PCD8544.h
@@ -8,6 +8,13 @@ enum PCD8544_DC {
     PCD8544_DC_DATA
 };
 
+typedef enum PCD8544_DisplayMode_t {
+    PCD8544_DISPLAY_MODE_ALL_WHITE = 0,
+    PCD8544_DISPLAY_MODE_ALL_BLACK = 1,
+    PCD8544_DISPLAY_MODE_NORMAL    = 4,
+    PCD8544_DISPLAY_MODE_INVERTED  = 5,
+} PCD8544_DisplayMode_t;
+
 #define PCD8544_FUNCTION_SET(_PD_, _V_, _H_) (0x20 | (0x07 & ((_PD_ << 2) | (_V_ << 1) | _H_)))
 #define PCD8544_DISPLAY_CONTROL(_D_, _E_)    (0x08 | (0x05 & ((_D_ << 2) | _E_)))
 #define PCD8544_VOP(_vop_)                   (0x80 | (0x7F & _vop_))
@@ -16,8 +23,15 @@ enum PCD8544_DC {
 
 class PCD8544 {
 private:
-    void (* setMode) (uint8_t);
-    void (* setData) (uint8_t);
+    void (* _setMode) (uint8_t);
+    void (* _setData) (uint8_t);
+
+    /**
+     * Set display mode
+     *
+     * @param mode
+     */
+    void _setDisplayMode(PCD8544_DisplayMode_t mode);
 public:
     /**
      * Instantiate driver with specific interface adapter functions
@@ -31,6 +45,13 @@ public:
      * @TODO maybe pass specific parameters
      */
     void initialize();
+
+    /**
+     * Set display mode
+     *
+     * @param mode
+     */
+    void setDisplayMode(PCD8544_DisplayMode_t mode);
 
     /**
      * Select display column (0...83)
