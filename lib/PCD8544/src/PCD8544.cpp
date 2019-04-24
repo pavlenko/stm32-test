3fd04d2cbd5b271f7ae9c9686e44c43e9247d37e
Pavlenko Eugeniy <pavlenko.obs@gmail.com>
2019-04-24 15:59:45 +0300

    add driver code

--- lib/PCD8544/src/PCD8544.cpp
+++ lib/PCD8544/src/PCD8544.cpp
@@ -1,1 +1,26 @@
 #include "PCD8544.h"
+
+PCD8544::PCD8544(void (* setMode) (uint8_t), void (* setData) (uint8_t)): setMode(setMode), setData(setData)
+{
+    this->setMode(PCD8544_DC_COMMAND);  // <-- Set command transfer mode
+
+    this->setData(PCD8544_FUNCTION_SET(0, 0, 1));  // <-- Tell LCD extended commands follow
+    this->setData(PCD8544_VOP(0x30));              // <-- Set LCD Vop (Contrast)
+    this->setData(PCD8544_TEMP_COEFFICIENT(0x03)); // <-- Set Temp coefficient
+    this->setData(PCD8544_BIAS(0x04));             // <-- Set LCD bias mode 1:48 (can try 0x03)
+
+    this->setData(PCD8544_FUNCTION_SET(0, 0, 0));  // <-- Tell LCD normal commands follow
+    this->setData(PCD8544_DISPLAY_CONTROL(1, 0));  // <-- Set display normal mode, not inverse
+}
+
+void PCD8544::setCol(uint8_t col)
+{
+    this->setMode(PCD8544_DC_COMMAND);
+    this->setData(0x80 | (0x07 & col));
+}
+
+void PCD8544::setRow(uint8_t row)
+{
+    this->setMode(PCD8544_DC_COMMAND);
+    this->setData(0x40 | (0x3F & row));
+}
--- lib/PCD8544/src/PCD8544.h
+++ lib/PCD8544/src/PCD8544.h
@@ -3,14 +3,41 @@
 
 #include <stdint.h>
 
+enum PCD8544_DC {
+    PCD8544_DC_COMMAND,
+    PCD8544_DC_DATA
+};
+
+#define PCD8544_FUNCTION_SET(_PD_, _V_, _H_) (0x20 | (0x07 & ((_PD_ << 2) | (_V_ << 1) | _H_)))
+#define PCD8544_DISPLAY_CONTROL(_D_, _E_)    (0x08 | (0x05 & ((_D_ << 2) | _E_)))
+#define PCD8544_VOP(_vop_)                   (0x80 | (0x7F & _vop_))
+#define PCD8544_TEMP_COEFFICIENT(_tc_)       (0x04 | (0x03 & _tc_))
+#define PCD8544_BIAS(_bias_)                 (0x10 | (0x07 & _bias_))
+
 class PCD8544 {
 private:
-    uint8_t port;
+    void (* setMode) (uint8_t);
+    void (* setData) (uint8_t);
 public:
-    PCD8544()
-    {
-        this->port = 1;
-    }
+    /**
+     * @param setMode
+     * @param setData
+     */
+    PCD8544(void (* setMode) (uint8_t), void (* setData) (uint8_t));
+
+    /**
+     * Select display column (0...83)
+     *
+     * @param col
+     */
+    void setCol(uint8_t col);
+
+    /**
+     * Select display row (0...5)
+     *
+     * @param row
+     */
+    void setRow(uint8_t row);
 };
 
 #endif //PCD8544_H
